edc7ee48f0589979d5a859e24f88cbd7945e3062
Nehuen <n.donozo@alumno.um.edu.ar>
2026-04-27 16:47:52 -0300

    Add libroExistenteException

---
+++ src/com/bibliotech/exception/LibroExistenteException.java
@@ -0,0 +1,7 @@
+package com.bibliotech.exception;
+
+public class LibroExistenteException extends RuntimeException {
+    public LibroExistenteException(String message) {
+        super(message);
+    }
+}
