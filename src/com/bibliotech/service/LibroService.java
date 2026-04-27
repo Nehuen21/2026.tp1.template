1ec63651acc9244aec6a9abdc40ea1a428433821
Nehuen <n.donozo@alumno.um.edu.ar>
2026-04-27 16:58:22 -0300

    add Libro service

---
+++ src/com/bibliotech/service/LibroService.java
@@ -0,0 +1,11 @@
+package com.bibliotech.service;
+
+import com.bibliotech.model.Libro;
+import com.bibliotech.repository.LibroRepository;
+
+public class LibroService {
+    private LibroRepository repositorio = new LibroRepository();
+    public void registrar(Libro libro ){
+
+    }
+}
