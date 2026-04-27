dc54ab2d8ff138f4a9e69f1e58102a81d3446761
Nehuen <n.donozo@alumno.um.edu.ar>
2026-04-27 15:58:17 -0300

    feat: add Categoria enum with id and nombre

--- src/com/bibliotech/model/Categoria.java
+++ src/com/bibliotech/model/Categoria.java
@@ -1,4 +1,19 @@
 package com.bibliotech.model;
 
 public enum Categoria {
+    CIENCIA(1,"Ciencia"),
+    HISTORIA(2,"Historia"),
+    CIENCIAS_BASICAS(3,"Ciencias basicas"),
+    ARTE(4,"ARTE");
+
+
+    private final int id;
+    private final String nombre;
+    Categoria(int id,String nombre){
+        this.id = id;
+        this.nombre = nombre;
+
+    }
+    public int getId(){return id;}
+    public String getNombre(){return nombre;}
 }
