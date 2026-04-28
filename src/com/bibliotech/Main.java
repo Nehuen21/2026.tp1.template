1cf1c0e89e0576f9390494f75232ee047afab534
Nehuen <n.donozo@alumno.um.edu.ar>
2026-04-28 12:20:26 -0300

    add LibroFisico and Ebook support in repository, service in main

--- src/com/bibliotech/Main.java
+++ src/com/bibliotech/Main.java
@@ -1,7 +1,9 @@
 package com.bibliotech;
 
 import com.bibliotech.model.Categoria;
+import com.bibliotech.model.Ebook;
 import com.bibliotech.model.Libro;
+import com.bibliotech.model.LibroFisico;
 import com.bibliotech.service.LibroService;
 
 public class Main {
@@ -11,8 +13,14 @@ public class Main {
         Libro libro = new Libro("978-3-16-148410-0", "El principito","Antoine de Saint-Exepury",1943, Categoria.ARTE);
         servicio.registrar(libro);
         System.out.println("Libro registrado correctamente ! ");
+        LibroFisico fisico = new LibroFisico("978-1","cien años","Garcia marquez",1967,Categoria.ARTE,"bueno","Estanteria A");
+        Ebook ebook = new Ebook("978","Percy Jackson","Facundo Lucero",2010,Categoria.CIENCIA,"PDF", 500000L);
+
+        servicio.registrar(fisico);
+        servicio.registrar(ebook);
     }
 }
+
 
 
 // java -cp out com.bibliotech.Main
